aa39dde443da381c4999ba63ceeccb72d484a896
Marcelo Martinho <martinho.guilherme13@hotmail.com>
2024-07-04 11:12:30 -0300

    changes

---
+++ DOCUMENTATION/page-courses-single-3.php
@@ -0,0 +1,248 @@
+<?php get_header(); ?>
+<!-- BANNER -->
+<div class="section banner-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<div class="title-page">Services</div>
+			</div>
+		</div>
+	</div>
+	<div class="breadcrumb-container">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="services.html">Cursos</a></li>
+				<li class="active">Life Coaching</li>
+			</ol>
+		</div>
+	</div>
+</div>
+
+<!-- CONTENT -->
+<div class="section section-border">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-6 col-md-6">
+				<h3>O que é Coaching relacionamento?</h3>
+				<p>
+					O Curso de Coaching de Relacionamento abrange uma ampla gama de temas essenciais para promover
+					relacionamentos saudáveis e eficazes. Desde a introdução aos fundamentos do coaching até técnicas
+					avançadas de comunicação e resolução de problemas, cada módulo equipa os participantes com
+					habilidades práticas para orientar indivíduos e casais em direção a relacionamentos mais
+					satisfatórios.
+				</p>
+				<p>
+					O curso enfatiza a importância da escuta ativa, compreensão emocional e
+					desenvolvimento pessoal contínuo, preparando os coachs para enfrentar desafios comuns e promover o
+					crescimento interpessoal em suas práticas de coaching.
+
+				</p>
+			</div>
+
+			<div class="col-sm-6 col-md-6">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/coachingrelacionamento.png" alt=""
+					class="img-responsive img-rounded">
+			</div>
+
+		</div>
+		<div class="spacer-50"></div>
+		<div class="row">
+			<div class="col-sm-6 col-md-6">
+				<h3>Conheça os Módulos do Curos</h3>
+				<div class="section-subheading">
+					O curso Coaching relacionamento é dividido em Módulos para poder facilitar o
+					entendimento e a aplicação das metodologias de ensinamento. <br>
+					conheça cada Módulo:
+				</div>
+				<ul class="checklist">
+					<ul>
+						<li><strong>Módulo I:</strong> Introdução ao Coaching de Relacionamento e Fundamentos Essenciais
+						</li>
+						<li><strong>Módulo II:</strong> Competências Fundamentais do Coach de Relacionamento e Ética
+							Profissional</li>
+						<li><strong>Módulo III:</strong> Estratégias Avançadas para Coaching de Relacionamento e
+							Desenvolvimento de Nichos</li>
+						<li><strong>Módulo IV:</strong> Comunicação Eficaz e Manejo de Conflitos em Relacionamentos</li>
+						<li><strong>Módulo V:</strong> Marketing e Desenvolvimento de Negócios para Coaches de
+							Relacionamento</li>
+						<li><strong>Módulo VI:</strong> Habilidades Avançadas de Escuta e Empoderamento no Coaching de
+							Relacionamento</li>
+						<li><strong>Módulo VII:</strong> Definição de Objetivos e Estratégias para Realização de Metas
+							em Relacionamentos</li>
+						<li><strong>Módulo VIII:</strong> Preparação e Condução de Sessões de Coaching Iniciais em
+							Relacionamento</li>
+						<li><strong>Módulo IX:</strong> Mindfulness e Bem-Estar em Relacionamentos</li>
+						<li><strong>Módulo X:</strong> Avaliação e Progresso Contínuo em Sessões de Coaching de
+							Relacionamento</li>
+						<li><strong>Módulo XI:</strong> Estabelecimento de Relações Duradouras e Gestão de Desafios
+							Pessoais</li>
+						<li><strong>Módulo XII:</strong> Sucesso Pós-Coaching e Manutenção de Relacionamentos Saudáveis
+						</li>
+						<li><strong>Módulo XIII:</strong> Avaliação de Personalidade e Uso de Ferramentas de Avaliação
+							em Coaching</li>
+						<li><strong>Módulo XIV:</strong> Coaching de Grupo em Relacionamentos e Facilitação de Dinâmicas
+							de Grupo</li>
+					</ul>
+
+				</ul>
+			</div>
+			<div class="col-sm-6 col-md-6">
+				<h3>Formato do Curso:</h3>
+				<p>O curso será desenvolvido da seguinte mandeira:</p>
+				<ul class="bull">
+					<li>Consulta de Coaching complementar de 30 a 60 minutos</li>
+					<li>As sessões duram de 30 a 90 minutos via Telefone/Skype ou Facetime/Presencial/Chat do Facebook
+						(a duração e a frequência das sessões dependem do pacote selecionado pelo cliente)</li>
+					<li>Suporte ilimitado por e-mail ou chat do Facebook</li>
+					<li>Chamadas ou mensagens breves ocasionais para check-in</li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="spacer-50"></div>
+
+		<!-- <div class="row">
+			<div class="col-sm-12 col-md-12">
+				<div class="panel-group panel-faq" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default active">
+						<div class="panel-heading" role="tab" id="heading1">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse1"
+									aria-expanded="true" aria-controls="collapse1">
+									Quisque dignissim vulputate purus in sodales?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse1" class="panel-collapse collapse in" role="tabpanel"
+							aria-labelledby="heading1" aria-expanded="true">
+							<div class="panel-body">
+								<p>Create and publilsh dynamic websites for desktop, tablet, and mobile devices that
+									meet the latest web standards- without writing code. Design freely using familiar
+									tools and hundreds of web fonts. easily add interactivity, including slide shows,
+									forms, and more.</p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading2">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+									Nam porttitor aliquet eros bibendum blandit?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2"
+							aria-expanded="false">
+							<div class="panel-body">
+								<p>When you click "Buy" button you'll be directed to our web store "Themeforest". You
+									can purchase our template there, and you will given permission to download our
+									templates.</p>
+								<ul class="list-unstyled">
+									<li><i class="fa fa-check"></i> Ready for all devices.</li>
+									<li><i class="fa fa-check"></i> HTML template</li>
+									<li><i class="fa fa-check"></i> Made with Bootstrap Framework.</li>
+									<li><i class="fa fa-check"></i> Easy Costumizable.</li>
+									<li><i class="fa fa-check"></i> Affordable Price.</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading3">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapse3" aria-expanded="false" aria-controls="collapse3">
+									Nullam non vehicula neque. Vivamus sed consectetur nunc. Proin eu tristique justo,
+									id eleifend urna?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3"
+							aria-expanded="false">
+							<div class="panel-body">
+								<p>Unzip the file, locate muse file and double click the file and you will directly to
+									adobe muse. Next step you can modifications our template, you can customize color,
+									text, font, content, logo and image with your need using familiar tools on adobe
+									muse without writing any code.</p>
+								<p>You can't re-distribute the Item as stock, in a tool or template, or with source
+									files. You can't re-distribute or make available the Item as-is or with superficial
+									modifications. These things are not allowed even if the re-distribution is for Free.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading4">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapse4" aria-expanded="false" aria-controls="collapse4">
+									Quisque feugiat non justo id porta?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading4"
+							aria-expanded="false">
+							<div class="panel-body">
+								<p>Open muse file, on the top bar adobe muse you will see text tool. click and add web
+									font do you want. Over 100+ typekit web font ready to use.</p>
+								<p>You just need open "asset" bar on the right side adobe muse. Next right click on the
+									image asset, then "Relink" image asset with your image fit. All image automatically
+									change on tablet and phone too. it's fast and simple. we make your life easier.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> -->
+
+		<!--TABELA DE PREÇO: -->
+		<div class="spacer-50"></div>
+
+	<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<h3>NOSSOS PREÇOS:</h3>
+				<div class="row">
+					<div class="col-sm-4 col-md-4">
+						<!-- ITEM 1 -->
+						<div class="panel panel-pricing">
+							<header class="panel-heading">
+								<h3>Parcelado</h3>
+								<div class="price">
+									<sup>R$</sup>99.50<small>12x</small>
+								</div>
+							</header>
+							<!-- BOTÃO DE COMPRA -->
+							<div class="panel-footer">
+								<a href="https://pay.hotmart.com/K21388972S" target="_blank" class="btn btn-primary">COMPRAR AGORA</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4 col-md-4">
+						<!-- ITEM 2 -->
+						<div class="panel panel-pricing best">
+							<header class="panel-heading">
+								<h3>à Vista</h3>
+								<div class="price">
+									<sup>R$</sup>997.00<small>à vista</small>
+								</div>
+							</header>
+							
+							<div class="panel-footer">
+								<a href="https://pay.hotmart.com/K21388972S" target="_blank" class="btn btn-primary">COMPRAR AGORA</a>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
