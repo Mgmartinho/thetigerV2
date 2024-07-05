8dffab9404ea42442f0383bc6f6254444b4bd0c0
nick212 <nick_carvalho29@hotmail.com>
2024-07-05 18:30:52 -0300

    fix projetct

---
+++ footer.php
@@ -0,0 +1,138 @@
+<?php wp_footer(); ?>
+
+
+<div class="section cta section-no-padding pd30">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<div class="cta-1">
+					<div class="body-text">
+						<h3>Pronto para começar?</h3>
+					</div>
+					<div class="body-action">
+						<a href="/cursos" class="btn btn-primary">CLIQUE AQUI!</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- FOOTER SECTION -->
+<div class="footer">
+
+	<div class="container justify-content-center">
+
+		<div class="row text-center">
+			<div class="col-sm-4 col-md-4">
+				<div class="footer-item">
+					<img src="<?php echo get_template_directory_uri(); ?>/images/logo1.png" style="height:120px;
+								width:225px" alt="logo bottom" class="logo-bottom">
+					<div class="spacer-30"></div>
+					<p>Rua Bomsucesso, 1118 - Tatuapé. <br>São Paulo, SP, Brazil, 03305-000.<br>
+						<br>
+						jeanpenelantoine@outlook.com <br>
+						+971 58 572 9927
+					</p>
+					<div class="footer-sosmed">
+						<a href="https://www.facebook.com/tigercoachaj" title="">
+							<div class="item">
+								<i class="fa fa-facebook"></i>
+							</div>
+						</a>
+						<a href="#" title="">
+							<div class="item">
+								<i class="fa fa-twitter"></i>
+							</div>
+						</a>
+						<a href="https://www.instagram.com/explore/tags/tigeruniversalcoaching/" title="">
+							<div class="item">
+								<i class="fa fa-instagram"></i>
+							</div>
+						</a>
+						<a href="https://wa.me/971585729927" target="_blank">
+							<div class="item">
+								<i class="fa fa-whatsapp"></i>
+							</div>
+						</a>
+
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-4">
+				<div class="footer-item">
+					<div class="footer-title">
+						NOSSA MISSÃO
+					</div>
+					<ul class="recent-post">
+						<li><a href="#" title="">Transformar vidas promovendo comunicação, intimidade e autoconfiança,
+								ajudando indivíduos a superar desafios e alcançar seu potencial com segurança financeira
+								e estratégias personalizadas.</a>
+
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-4">
+				<div class="footer-item">
+					<div class="footer-title">
+						PAGINAS
+					</div>
+					<ul class="list">
+						<li><a href="/" title="">HOME</a></li>
+						<li><a href="/sobrenos" title="">SOBRE NÓS</a>
+						</li>
+						<li><a href="/cursos" title="">CURSOS</a>
+						</li>
+						<li><a href="/contato" title="">CONTATO</a></li>
+						<li><a href="/login" title="">PORTAL DO
+								ALUNO</a></li>
+					</ul>
+				</div>
+			</div>
+			<!-- adicionar mais campos no rodape -->
+		</div>
+	</div>
+
+	<div class="fcopy">
+		<div class="container text-center">
+			<div class="row">
+				<div class="col-sm-12 col-md-12">
+					<p class="ftex">&copy; Todos os Direitos Reservados</p>
+				</div>
+			</div>
+		</div>
+	</div>
+
+</div>
+
+<!-- BOTÃO WHATSAPP -->
+<a href="https://wa.me/971585729927" class="whatsapp-button" target="_blank">
+	<i class="fa fa-whatsapp" style="font-size: 30px;"></i>
+</a>
+
+
+<!-- JS VENDOR -->
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/vendor/bootstrap.min.js"></script>
+<script type="text/javascript"
+	src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.superslides.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/vendor/owl.carousel.js"></script>
+<script type="text/javascript"
+	src="<?php echo get_template_directory_uri(); ?>/js/vendor/bootstrap-dropdownhover.min.js"></script>
+<script type="text/javascript"
+	src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery.magnific-popup.min.js"></script>
+
+<!-- SENDMAIL -->
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/vendor/validator.min.js"></script>
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/vendor/form-scripts.js"></script>
+
+<script type='text/javascript' src='https://maps.google.com/maps/api/js?sensor=false&#038;ver=4.1.5'></script>
+
+<script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/script.js"></script>
+
+
+
+</body>
+
+</html>
---
+++ page-courses-single-1.php
@@ -0,0 +1,244 @@
+<?php get_header(); ?>
+<!-- BANNER -->
+<div class="section banner-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<div class="title-page">Services</div>
+			</div>
+		</div>
+	</div>
+	<div class="breadcrumb-container">
+		<div class="container">
+			<ol class="breadcrumb">
+				<li><a href="index.html">Home</a></li>
+				<li><a href="services.html">Cursos</a></li>
+				<li class="active">Life Coaching</li>
+			</ol>
+		</div>
+	</div>
+</div>
+
+<!-- CONTENT -->
+<div class="section section-border">
+	<div class="container">
+		<div class="row">
+
+			<div class="col-sm-6 col-md-6">
+				<h3>O que é Life Coaching?</h3>
+				<p>
+					Life Coaching é uma parceria colaborativa projetada entre o coach e o cliente, onde a relação de
+					coaching devolve continuamente todo o poder a você, o cliente. Eu acredito que você conhece as
+					respostas para todas as perguntas ou desafios que possa ter em sua vida, mesmo que essas respostas
+					pareçam estar obscurecidas, ocultas ou escondidas dentro de você. Eu aplico técnicas baseadas em
+					pesquisas para ajudar as pessoas a desenvolverem hábitos mais eficazes e a superarem os desafios da
+					vida.
+				</p>
+				<p>
+					Por que ser uma pessoa comum quando você pode ser um super-herói?
+					A vida é melhor quando você está no topo.
+					Quer sucesso ou mais sucesso?
+					Você precisa de um coach.
+					O objetivo do coaching é orientar a visão, estimular e capacitar o que está sendo treinado - o
+					jogador - através do estabelecimento de uma conexão mais firme com a sua autoridade interior.
+					Coaching ajuda a definir e alcançar seus objetivos Emocionantes.
+					Coaching é ajudar os outros a identificar e definir os seus objetivos específicos, e em seguida,
+					organizar-se para atingir esses objetivos. É a construção de habilidades pessoais de um indivíduo
+					ajudando-o a alcançar seus objetivos.
+					Coaches baseiam-se em conhecimento interior de um cliente com recursos e criatividade para ajudá-lo
+					a ser mais eficaz.
+
+				</p>
+			</div>
+
+			<div class="col-sm-6 col-md-6">
+				<img src="<?php echo get_template_directory_uri(); ?>/images/lifecoach.png" alt=""
+					class="img-responsive img-rounded">
+			</div>
+
+		</div>
+		<div class="spacer-50"></div>
+		<div class="row">
+			<div class="col-sm-6 col-md-6">
+				<h3>Conheça os Módulos do Curos</h3>
+				<div class="section-subheading">
+					O curso de Life Coach é dividido em Módulos para poder facilitar o
+					entendimento e a aplicação das metodologias de ensinamento. <br>
+					conheça cada Módulo:
+				</div>
+				<ul class="checklist">
+					<li>Módulo I: Introdução aos conceitos e princípios fundamentais do coaching, incluindo definições e
+						perguntas frequentes.</li>
+					<li>Módulo II:
+						Exploração dos requisitos e competências da Federação Internacional de Coaching (ICF) para
+						certificação.</li>
+					<li>Módulo III:
+						Estratégias para atrair clientes, desenvolver confiança como coach e configurar seu escritório.
+					</li>
+					<li>Módulo IV:
+						Práticas éticas e profissionais no coaching, distinguindo entre coaching e outras profissões de
+						apoio.</li>
+					<li>Módulo V:
+						Técnicas de marketing para coaches e a importância de fazer perguntas poderosas no processo de
+						coaching.</li>
+
+				</ul>
+			</div>
+			<div class="col-sm-6 col-md-6">
+				<h3>Formato do Curso:</h3>
+				<p>O curso será desenvolvido da seguinte mandeira:</p>
+				<ul class="bull">
+					<li>Consulta de Coaching complementar de 30 a 60 minutos</li>
+					<li>As sessões duram de 30 a 90 minutos via Telefone/Skype ou Facetime/Presencial/Chat do Facebook
+						(a duração e a frequência das sessões dependem do pacote selecionado pelo cliente)</li>
+					<li>Suporte ilimitado por e-mail ou chat do Facebook</li>
+					<li>Chamadas ou mensagens breves ocasionais para check-in</li>
+				</ul>
+			</div>
+
+		</div>
+
+		<div class="spacer-50"></div>
+
+		<!-- <div class="row">
+			<div class="col-sm-12 col-md-12">
+				<div class="panel-group panel-faq" id="accordion" role="tablist" aria-multiselectable="true">
+					<div class="panel panel-default active">
+						<div class="panel-heading" role="tab" id="heading1">
+							<h4 class="panel-title">
+								<a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse1"
+									aria-expanded="true" aria-controls="collapse1">
+									Quisque dignissim vulputate purus in sodales?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse1" class="panel-collapse collapse in" role="tabpanel"
+							aria-labelledby="heading1" aria-expanded="true">
+							<div class="panel-body">
+								<p>Create and publilsh dynamic websites for desktop, tablet, and mobile devices that
+									meet the latest web standards- without writing code. Design freely using familiar
+									tools and hundreds of web fonts. easily add interactivity, including slide shows,
+									forms, and more.</p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading2">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapse2" aria-expanded="false" aria-controls="collapse2">
+									Nam porttitor aliquet eros bibendum blandit?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading2"
+							aria-expanded="false">
+							<div class="panel-body">
+								<p>When you click "Buy" button you'll be directed to our web store "Themeforest". You
+									can purchase our template there, and you will given permission to download our
+									templates.</p>
+								<ul class="list-unstyled">
+									<li><i class="fa fa-check"></i> Ready for all devices.</li>
+									<li><i class="fa fa-check"></i> HTML template</li>
+									<li><i class="fa fa-check"></i> Made with Bootstrap Framework.</li>
+									<li><i class="fa fa-check"></i> Easy Costumizable.</li>
+									<li><i class="fa fa-check"></i> Affordable Price.</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading3">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapse3" aria-expanded="false" aria-controls="collapse3">
+									Nullam non vehicula neque. Vivamus sed consectetur nunc. Proin eu tristique justo,
+									id eleifend urna?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading3"
+							aria-expanded="false">
+							<div class="panel-body">
+								<p>Unzip the file, locate muse file and double click the file and you will directly to
+									adobe muse. Next step you can modifications our template, you can customize color,
+									text, font, content, logo and image with your need using familiar tools on adobe
+									muse without writing any code.</p>
+								<p>You can't re-distribute the Item as stock, in a tool or template, or with source
+									files. You can't re-distribute or make available the Item as-is or with superficial
+									modifications. These things are not allowed even if the re-distribution is for Free.
+								</p>
+							</div>
+						</div>
+					</div>
+					<div class="panel panel-default">
+						<div class="panel-heading" role="tab" id="heading4">
+							<h4 class="panel-title">
+								<a class="collapsed" role="button" data-toggle="collapse" data-parent="#accordion"
+									href="#collapse4" aria-expanded="false" aria-controls="collapse4">
+									Quisque feugiat non justo id porta?
+								</a>
+							</h4>
+						</div>
+						<div id="collapse4" class="panel-collapse collapse" role="tabpanel" aria-labelledby="heading4"
+							aria-expanded="false">
+							<div class="panel-body">
+								<p>Open muse file, on the top bar adobe muse you will see text tool. click and add web
+									font do you want. Over 100+ typekit web font ready to use.</p>
+								<p>You just need open "asset" bar on the right side adobe muse. Next right click on the
+									image asset, then "Relink" image asset with your image fit. All image automatically
+									change on tablet and phone too. it's fast and simple. we make your life easier.</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> -->
+
+		<!--TABELA DE PREÇO: -->
+		<div class="spacer-50"></div>
+
+		<div class="row">
+			<div class="col-sm-12 col-md-12">
+				<h3>NOSSOS PREÇOS:</h3>
+				<div class="row">
+					<div class="col-sm-4 col-md-4">
+						<!-- ITEM 1 -->
+						<div class="panel panel-pricing">
+							<header class="panel-heading">
+								<h3>Parcelado</h3>
+								<div class="price">
+									<sup>R$</sup>99.50<small>12x</small>
+								</div>
+							</header>
+							<!-- BOTÃO DE COMPRA -->
+							<div class="panel-footer">
+								<a href="https://pay.hotmart.com/P6057267L?bid=1719757038923" target="_blank" class="btn btn-primary">COMPRAR AGORA</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-4 col-md-4">
+						<!-- ITEM 2 -->
+						<div class="panel panel-pricing best">
+							<header class="panel-heading">
+								<h3>à Vista</h3>
+								<div class="price">
+									<sup>R$</sup>997.00<small>à vista</small>
+								</div>
+							</header>
+							
+							<div class="panel-footer">
+								<a href="https://pay.hotmart.com/P6057267L?bid=1719757038923" target="_blank" class="btn btn-primary">COMPRAR AGORA</a>
+							</div>
+						</div>
+					</div>
+					
+				</div>
+			</div>
+
+		</div>
+
+	</div>
+</div>
+
+<?php get_footer(); ?>
